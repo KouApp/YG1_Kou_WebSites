9805131a7a71a4b6aaa33dfb4028d45c1c6dbbd7
siracarapoglu <arapoglusirac@gmail.com>
2021-12-27 19:23:17 +0300

    Cap Basvuru Sayfası
    cap basvuru sayfası tamamlandı

--- public/userCAP.php
+++ public/userCAP.php
@@ -3,16 +3,17 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>İntibak Başvurusu</title>
-    <link rel="stylesheet" href="css/intibak.css">
+    <title>ÇAP Başvurusu</title>
+    <link rel="stylesheet" href="css/userCAP.css">
 </head>
+
 <body>
-<?php
+    <?php
     if (strlen($_SESSION['user']) == 0) {
         header("Location: http://127.0.0.1:8000/login.php");
     }
@@ -35,17 +36,19 @@ session_start();
 
     <div class="container">
         <div class="ygb-header">
-            İNTİBAK BAŞVURUSU
+            ÇİFT ANADAL PROGRAMI BAŞVURUSU
         </div>
 
         <div class="row">
             <p>Ad Soyad:</p>
             <input type="text" placeholder="Ad Soyad">
         </div>
+
         <div class="row">
             <p>Öğrenci No:</p>
             <input type="text" placeholder="Öğrenci No">
         </div>
+
         <div class="row">
             <p>Fakülte:</p>
             <select name="select" id="select">
@@ -57,38 +60,66 @@ session_start();
                 <option value="5">Mühendislik Fakültesi</option>
             </select>
         </div>
+
         <div class="row">
             <p>Bölüm:</p>
             <select name="select" id="select">
                 <option selected disabled>Seçim Yapınız</option>
-                <option value="1">Bölüm 1</option>
-                <option value="2">Bölüm 2</option>
+                <option value="1">Yasin</option>
+                <option value="2">Buraya</option>
+                <option value="3">Da</option>
+                <option value="4">Yardım</option>
+                <option value="5">Lazım</option>
             </select>
         </div>
+
         <div class="row">
-            <p>Kayıtlı Fakülte:</p>
-            <input type="text" placeholder="Kayıtlı Fakülte">
+            <p>Program:</p>
+            <input type="text" placeholder="Program">
         </div>
+
         <div class="row">
-            <p>Geçiş Türü:</p>
-            <input type="text" placeholder="Kayıtlı Bölüm">
+            <p>Öğrenim Türü:</p>
+            <input type="text" placeholder="Öğrenim Türü">
         </div>
+
         <div class="row">
-            <p>Mevcut Yıl:</p>
-            <input type="text" placeholder="Yerleştiği Yıl">
+            <p>Telefon:</p>
+            <input type="text" placeholder="Telefon">
         </div>
+
         <div class="row">
-            <p>Mevcut Yarıyıl:</p>
-            <input type="text" placeholder="Başvurulan Fakülte">
+            <p>E-posta:</p>
+            <input type="text" placeholder="E-posta">
         </div>
+
         <div class="row">
-            <p>İntibak Edilecek Yarıyıl:</p>
-            <input type="text" placeholder="Başvurulan Bölüm">
+            <p>Adres:</p>
+            <input type="text" placeholder="Adres">
         </div>
+
+        <div class="row">
+            <p>İstenilen Bölüm:</p>
+            <select name="select" id="select">
+                <option selected disabled>Seçim Yapınız</option>
+                <option value="1">Yasin</option>
+                <option value="2">Buraya</option>
+                <option value="3">Da</option>
+                <option value="4">Yardım</option>
+                <option value="5">Lazım</option>
+            </select>
+        </div>
+
+        
         <div class="file-container">
-            <input href="" class="apply-button" type="submit" value="Dilekçe Oluştur">
-            <input href="" class="apply-button" type="submit" value="Dilekçe Yolla">
+            <input href="" id="fileButton" type="submit" value="Belge Gönder">
+            <input href="" id="transkriptButton" type="submit" value="Transkript">
+        </div>
+
+        <div class="file-container2">
+            <input href="" id="applyButton" class="apply-button" type="submit" value="Başvur">
         </div>
     </div>
 </body>
+
 </html>
