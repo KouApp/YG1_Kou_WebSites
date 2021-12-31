7751eb884914bbab3f9799784954345d15558524
emrecetindemir <55704322+emrecetindemir@users.noreply.github.com>
2021-12-31 16:44:30 +0300

    sifre sıfırlama
    sifre sıfırlama ekranları yapıldı. kayıt sayfalarının title'ı düzenlendi.

--- public/kayitBasarili.php
+++ public/kayitBasarili.php
@@ -8,7 +8,7 @@ header("Refresh: 3; url=login.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>404</title>
+    <title>Kayıt Başarılı</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&family=Roboto:ital,wght@0,400;0,500;1,300&display=swap" rel="stylesheet">
--- public/sifreBasarisiz.php
+++ public/sifreBasarisiz.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-header("Refresh: 3; url=login.php");
+header("Refresh: 5; url=login.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,7 +8,7 @@ header("Refresh: 3; url=login.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kayıt Başarısız</title>
+    <title>Şifre Sıfırlanamadı</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&family=Roboto:ital,wght@0,400;0,500;1,300&display=swap" rel="stylesheet">
@@ -18,9 +18,9 @@ header("Refresh: 3; url=login.php");
 <body>
     <div class="container">
         <div class="content">
-            <h2>BAŞARISIZ!</h2>
-            <h4>Kayıt işlemin tamamlanamadı :(</h4>
-            <p>3 saniye içerisinde otomatik olarak giriş yapma ekranına yönlendirileceksin.</p>
+            <h2>BAŞARISIZ</h2>
+            <h4>Şifre sıfırlama işlemin tamamlanamadı :(</h4>
+            <p>5 saniye içerisinde otomatik olarak giriş yapma ekranına yönlendirileceksin.</p>
             <a href="login.php">Giriş Sayfası'na Dön</a>
         </div>
     </div>
