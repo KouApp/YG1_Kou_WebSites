753fab5ccb8a51816ee95c1991942c167ee72915
emrecetindemir <55704322+emrecetindemir@users.noreply.github.com>
2022-01-06 19:55:15 +0300

    menüler eklendi

--- public/userYAZ.php
+++ public/userYAZ.php
@@ -10,9 +10,13 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Yaz Okulu Başvurusu</title>
     <link rel="stylesheet" href="css/userYAZ.css">
+    <link rel="stylesheet" href="css/menu.css">
 </head>
 
 <body>
+    <div id="navigation">
+        <script src = "menu.js"></script>
+    </div>
     <?php
     if (strlen($_SESSION['user']) == 0) {
         header("Location: http://127.0.0.1:8000/login.php");
